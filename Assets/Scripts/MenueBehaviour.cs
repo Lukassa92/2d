812c51392e5d05da9b1b42ba23a65f60eda2046a
Lukassa <lukasschrdr@googlemail.com>
2018-09-22 18:48:19 +0200

    Add clean startproject

---
+++ Assets/Scripts/MenueBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenueBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public void LoadGameScene()
+    {
+        StartCoroutine(_LoadGameScene());
+    }
+
+    private IEnumerator _LoadGameScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("HomeTown");
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void LoadOptionsScene()
+    {
+        Debug.Log("Implentation later...!");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
