ba070bfd81e648761ffd1a12b4abf87978590872
Lukassa <lukasschrdr@googlemail.com>
2018-09-23 15:40:25 +0200

    Add rudimentary NearstTargetCheck

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class CharacterMovement: MonoBehaviour
 {
-    public float speed = 150.0f;
-    private bool _running = true;
-
+    public enum State
+    {
+        Run,
+        Fight,
+        Stand
+    }
+    public enum Direction
+    {
+        Left,
+        Right
+    }
+    private float _speed = 150.0f;
+    [SerializeField]
+    protected internal State CharacterState = State.Run;
+    
+    private Direction _direction = Direction.Right;
     private Rigidbody2D _rigidbody2D;
+
+    private Vector3 _rigidbody2DVelocity;
 	// Use this for initialization
 	void Start () {
 		
@@ -18,10 +33,76 @@ public class CharacterMovement: MonoBehaviour
         _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
+    public void StopMovement()
+    {
+        CharacterState = State.Stand;
+    }
+
+    public void Attack()
+    {
+        Debug.Log("Attacke!");
+        StopMovement();
+        CharacterState = State.Fight;
+        //Play attack animation
+        //start calculate damage at enemy and by you
+        StartCoroutine(DeleteEnemyForDev());
+    }
+
+    private IEnumerator DeleteEnemyForDev()
+    {
+        yield return new WaitForSeconds(3.0f);
+        Destroy(GameObject.Find("Enemy"));
+        Run();
+    }
+
+    public void Run()
+    {
+        CharacterState = State.Run;
+    }
+
     // Update is called once per frame
     private void Update () {
-        //Move Forward 
-        _rigidbody2D.velocity = new Vector2(speed, _rigidbody2D.velocity.y);
-		//_rigidbody2D.AddForce(transform.forward * speed);
+        //Move Forward
+        if (CharacterState == State.Run)
+        {
+            if (_direction == Direction.Right)
+            {
+                Debug.Log("Turn right");
+                _rigidbody2D.velocity = new Vector2(_speed, _rigidbody2D.velocity.y);
+            }
+            else
+            {
+                _rigidbody2D.velocity = new Vector2(-_speed, _rigidbody2D.velocity.y);
+            }
+            
+        }        
 	}
+
+    public void RunToTarget(float targetNearstEnemy)
+    {
+        if (targetNearstEnemy < transform.position.y)
+        {
+//            Hier muss sich der Character umdrehen
+            FlipChar();
+        }
+        else
+        {
+            FlipChar();
+        }
+        Run();
+    }
+
+    private void FlipChar()
+    {
+        if (_direction == Direction.Right)
+        {
+            _direction = Direction.Left;
+            transform.localScale = new Vector3(- transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+        else
+        {
+            _direction = Direction.Right;
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+    }
 }
--- Assets/Scripts/EnemyDetection.cs
+++ Assets/Scripts/EnemyDetection.cs
@@ -1,15 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyDetection : MonoBehaviour {
+    public class Target
+    {
+        public Vector3 Position { get; set; }
+
+        public string Name { get; set; }
+    }
+    public Target _nearstTarget;
+    public string NearstTargetName;
+    public List<Target> _targetCollisions = new List<Target>();
+    private CharacterMovement _characterMovement;
+    private bool _newTarget = false;
+
+    void Start()
+    {
+        _characterMovement = GameObject.Find("Fred").GetComponent<CharacterMovement>();
+    }
 
     void OnTriggerEnter2D(Collider2D collidor)
     {
         if (collidor.name == "UnitDetector")
         {
-            Debug.Log("Gegner gesichtet!");
+            Debug.Log("Gegner gesichtet! position: "+collidor.transform.position);
+            _targetCollisions.Add(new Target { Position = collidor.transform.position, Name = collidor.transform.name });
+            _newTarget = true;
         }
     }
-	
+
+    void Update()
+    {
+        if (_targetCollisions.Count != 0 && _characterMovement.CharacterState == CharacterMovement.State.Run && _newTarget)
+        {
+            _characterMovement.RunToTarget(TargetNearstEnemy());
+        }
+    }
+
+    private float TargetNearstEnemy()
+    {
+        float actualNearstContactDistance = 0.0f;
+        Target actualNearstContact = new Target();
+        foreach (var targetCollision in _targetCollisions)
+        {
+            if (actualNearstContactDistance == 0.0f)
+            {
+                actualNearstContact = targetCollision;
+            }
+            else if (Vector3.Distance(GetComponentInParent<Transform>().position, targetCollision.Position) < actualNearstContactDistance && _nearstTarget != targetCollision)
+            {
+                actualNearstContact = targetCollision;
+            }
+        }
+        _nearstTarget = actualNearstContact;
+        _newTarget = false;
+        //GetComponentInParent<Image>().color = new Color(35,35,35);
+        NearstTargetName = actualNearstContact.Name +" position: "+actualNearstContact.Position;
+        return actualNearstContact.Position.x;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        Debug.Log("Trigger exit by: "+col.transform.name);
+            RemoveTargetByName(col.transform.name);
+    }
+
+    private void RemoveTargetByName(string name)
+    {
+        Target obsoletTarget = null;
+        foreach (var targetCollision in _targetCollisions)
+        {
+            if (targetCollision.Name == name)
+            {
+                obsoletTarget = targetCollision;
+            }
+        }
+
+        if (obsoletTarget != null)
+        {
+            _targetCollisions.Remove(obsoletTarget);
+        }
+    }
 }
