5f2f4c8e6317ab3ba451ce3a54ab7472f81c0a29
André Dickenscheidt <gitkraken@dickenscheidt-it.de>
2018-09-23 21:32:34 +0200

    Added LevelEntity classes

---
+++ Assets/Scripts/Level/Classes/LevelEntity.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+public abstract class LevelEntity
+{
+    private int _health;
+    private bool _isAlive = true;
+    private int _baseMaxHealth;
+
+    public int BaseMaxHealth
+    {
+        get { return _baseMaxHealth; }
+        set
+        {
+            var val = value < 1 ? 1 : value;
+            _baseMaxHealth = val;
+        }
+    }
+
+    public int Health
+    {
+        get { return IsAlive ? _health : 0; }
+        set { _health = value > 0 ? value : 0; }
+    }
+
+    public int PhysicalResistance { get; set; }
+    public int FireResistance { get; set; }
+    public int ColdResistance { get; set; }
+    public int EnergyResistance { get; set; }
+    public int PoisonResistance { get; set; }
+
+    public bool IsAlive
+    {
+        get { return _isAlive; }
+        private set { _isAlive = value; }
+    }
+
+    public bool CanWalk { get; set; }
+    public bool IsVisible { get; set; }
+
+    public abstract EntityType Entity { get; }
+
+    public void DoDamage(DamageSource damageSource)
+    {
+        if (!OnBeforeDamaged(damageSource))
+        {
+            return;
+        }
+        var healthAfterDamage = Health - damageSource.Damage;
+        if (healthAfterDamage > 0 || !Kill(damageSource))
+        {
+            Health -= damageSource.Damage;
+            OnAfterDamaged(damageSource);
+        }
+    }
+
+    public virtual bool OnBeforeDamaged(DamageSource damageSource)
+    {
+        return true;
+    }
+
+    public abstract void OnAfterDamaged(DamageSource damageSource);
+
+    public bool Kill(DamageSource damageSource)
+    {
+        if (OnBeforeDeath(damageSource))
+        {
+            OnDeath();
+            Health = 0;
+            IsAlive = false;
+            OnAfterDeath();
+            return false;
+        }
+        return true;
+    }
+
+    public virtual bool OnBeforeDeath(DamageSource damageSource)
+    {
+        return true;
+    }
+    public abstract void OnDeath();
+    public abstract void OnAfterDeath();
+}
